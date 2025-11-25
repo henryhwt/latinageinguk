e085f312944df12ca2980217488812ecdafe50a0
henryhwt <102864464+henryhwt@users.noreply.github.com>
2025-11-25 19:09:21 +0000

    added pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+import { DEFAULT_LOCALE_SETTING, LOCALES_SETTING } from './src/locales';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    i18n: {
+    defaultLocale: DEFAULT_LOCALE_SETTING,
+    locales: Object.keys(LOCALES_SETTING),
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false,
+    },
+  },
+
+  //local host
+  site: 'http://localhost:432',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+
+});
